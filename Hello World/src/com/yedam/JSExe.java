928e47c0ca6731b7d799b7f019d6b4c95b1e2b6e
admin <toptensu@naver.com>
2025-05-14 17:23:11 +0900

    변수완료

--- Hello World/src/com/yedam/JSExe.java
+++ Hello World/src/com/yedam/JSExe.java
@@ -1,26 +1,91 @@
 package com.yedam;
 
+import java.util.Scanner;
+
 public class JSExe {
+
 	// main ctrl + space
 	public static void main(String[] args) {
-		test2();
+		int num1 = 30;
+		int num2 = 30;
+//		System.out.println(num1 == num2);
+
+		String str1 = new String("Hello");
+		String str2 = new String("Hello");
+//		System.out.println(str1);
+//		System.out.println(str2);
+
+//		System.out.println(str1.equals(str2));
+
+		// "30" vs .30 문자와 숫자 비교
+		String str3 = "30";
+		int num3 = 30;
+//		System.out.println(str3.equals(" " + num3));
+		int num4 = Integer.parseInt(str3); // 문자열 숫자
+//		System.out.println(num3 == num4); // 30 vs ."30" *> 30변경
+		// 비교 연산자 중 문자열을 비교할때는 equals 를 사용해야된다.
+
+//		test2();// 평균 구하기 소수점 자리 까지
+//		test3();// 스캐너 사용 
+		test4();
 	}// end of main
 
+	public static void test4() {
+		Scanner scn = new Scanner(System.in); // 사용자 입력을 위한 Scanner 생성
+		String str = "친구 목록은 "; // 누적할 문자열의 시작 부분 (변수선언)
+
+		while (true) {
+			System.out.println("친구이름 입력. 종료하려면 quit>>");
+			String msg = scn.nextLine(); // 사용자 입력 한 줄을 받음
+
+			if (msg.equals("quit")) { // 입력이 "quit"이면
+				str += "입니다"; // 문장을 마무리
+				break; // 반복 종료
+			}
+
+			str += msg + ", "; // 이름을 누적하고 쉼표로 구분 (변수 누적)
+			System.out.println("입력한 값은 " + msg); // 입력된 이름을 출력
+		}
+
+		System.out.println(str); // 최종 친구 목록 출력
+	}
+
+}// end of test4.
+
+	public static void test3() {
+		// 사용자의 입렵값을 읽어들이기
+		Scanner scn = new Scanner(System.in);// scanner 변수 알아보기
+		int sum = 0;
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("학생의 점수 입력>>");
+			String value = scn.nextLine(); // 입력값을 문자열형태로 반환.
+			int score = Integer.parseInt(value);
+			sum += score;
+		}
+		double avg = sum / 3.0;
+		avg = Math.round(avg * 100) / 100.0; // math.round 는 반올림 해주는것
+		System.out.println("합계:" + sum + "평균" + avg);
+
+	}// end of test3
+
 	public static void test2() {
-
-		//변경된 부분.
-
-
-
-		//아무내용이나 .....
-		// 임의의 수를 생성.1~100사이의 임의의 값 생성
+		// 변경된 부분.
+		// 아무내용이나 .....
+		// 임의의 수를 생성.30~100사이의 임의의 값 생성
+		// 평균: 173.0*1.0/5.0 =>34.654
 		// 1 <= x < 11
+
 		int sum = 0;
 		for (int i = 1; i <= 5; i++) {
-			int result = (int) (Math.random() * 100) + 1;
+			int result = (int) (Math.random() * 71) + 30;
+			System.out.println(result);
 			sum += result;
 		}
-		System.out.println("결과" + sum);
+		double avg = sum / 7.0;
+		avg = Math.round(avg * 100) / 100.0;
+		System.out.println("합계:" + sum + "평균" + avg);
+
+//		System.out.println(Math.round(23.678 * 100)/100.0);
 	} // end of test2.
 
 	public static void test() {
