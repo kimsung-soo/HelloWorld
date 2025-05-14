87a1f9233c60b49cbea5c6f6c8aad920ab45cdd0
admin <toptensu@naver.com>
2025-05-14 12:03:41 +0900

    복습

---
+++ Hello World/src/com/yedam/HelloJava.java
@@ -0,0 +1,23 @@
+package com.yedam;
+
+//crtl +shft+f =정렬
+public class HelloJava {
+	public static void main(String[] args) {
+	// int(정수), double(실수), boolean(참 거짓)
+	int num1 =10; //변수를 초기화.
+	double num2 =20; // 자동형변환(promotion)
+	
+	num1 = (int) num2; //강제형변환(casting)
+	
+	int num3 = 100;
+	double num4 = 200;
+	double result = (double) num3 + num4;
+	System.out.println("결과는" + result);
+	
+	//20 + 30 = 50
+	System.out.println("결과는" + (20 + 30));
+	//문자를 넣으면 결과는2030으로 표시 된다.
+	//문자를 넣지 않으면 50.0으로 표시 된다.  같은 데이터 타입으로 연산을 해줘야된다. ()을 쳐줘야 된다.
+	
+	}
+}
---
+++ Hello World/src/com/yedam/JSExe.java
@@ -0,0 +1,29 @@
+package com.yedam;
+
+public class JSExe {
+	// main ctrl + space
+	public static void main(String[] args) {
+		// 임의의 수를 생성.1~10사이의 임의의 값 생성
+		// 1 <= x < 11
+		int sum = 0;
+		for (int i = 1; i <= 5; i++) {
+			int result = (int) (Math.random() * 10) + 1;
+            sum += result;
+		}
+		System.out.println("결과" + sum);
+		
+	}// end of main
+
+	public static void test() {
+		int sum = 0;
+		// 1~10 까지의 누적하는 반복문 생성.
+		for (int i = 1; i <= 10; i++) {
+			// 홀수만 변수주기
+			if (i % 2 == 1) {
+				sum += i;
+			}
+		}
+		System.out.println("결과" + sum);
+	}// end of test
+
+}// end of class(JSExe)
