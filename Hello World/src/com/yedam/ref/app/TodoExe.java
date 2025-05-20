0713fdd4c6e1d6bf1c4c790be1525afb0370ff0d
admin <toptensu@naver.com>
2025-05-20 09:38:34 +0900

    del

--- Hello World/src/com/yedam/ref/app/TodoExe.java
+++ Hello World/src/com/yedam/ref/app/TodoExe.java
@@ -4,7 +4,7 @@ public class TodoExe {
 	public static void main(String[] args) {
 		int[][] numAry = new int[3][5]; //3행 5열짜리 배열, 즉 3x5 크기의 표
 		// 임의의 숫자 생성.
-		 int sum = 0;
+		
 		   
 		for (int outer = 0; outer < numAry.length; outer++) {
 			for (int inner = 0; inner < numAry[outer].length; inner++) {
@@ -12,6 +12,8 @@ public class TodoExe {
 					
 				System.out.printf(numAry[outer][inner]+"\t");
 				System.out.println();
+				
+				 int sum = 0;
 				
 				for (int o = 0; o < numAry.length; o++ ) {
 					for(int in = 0; in < numAry[o].length; in++) {
@@ -25,4 +27,4 @@ public class TodoExe {
 		}
 
 	}
-}
+}//end of class
